cae2fb76552def806e189f2f82dbedb80e664fa0
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-12-23 19:54:08 +0000

    using ISR for the fetch events

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,11 @@
-import Layout from "@/components/Layout";
+import Layout from '@/components/Layout'
+
 export default function AboutPage() {
   return (
-    <div>
-      <Layout title="About DJ Events">
-        <h1>About</h1>
-        <p>
-          This is an app to find the latest DJ and other musical events.
-          Version:
-        </p>
-        <p>1.0.0</p>
-      </Layout>
-    </div>
-  );
+    <Layout title='About DJ Events'>
+      <h1>About</h1>
+      <p>This is an app to find the latest DJ and other musical events</p>
+      <p>Version: 1.0.0</p>
+    </Layout>
+  )
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,6 @@
-import Layout from "@/components/Layout";
-import EventItem from "@/components/EventItem";
-import { API_URL } from "@/config/index";
+import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+import { API_URL } from '@/config/index'
 
 export default function EventsPage({ events }) {
   return (
@@ -12,15 +12,15 @@ export default function EventsPage({ events }) {
         <EventItem key={evt.id} evt={evt} />
       ))}
     </Layout>
-  );
+  )
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events`);
-  const events = await res.json();
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
 
   return {
     props: { events },
     revalidate: 1,
-  };
+  }
 }
