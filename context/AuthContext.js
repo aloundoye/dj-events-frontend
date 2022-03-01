f91dd623dc91217b73b92ac86c67a630ce1c70f4
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-03-01 14:19:04 +0000

    Login

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,21 +1,38 @@
-import {createContext, useState, useEffect} from "react";
-import {useRouter} from "next/router";
-import {API_URL} from "@/config/index";
+import { createContext, useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { NEXT_URL } from "@/config/index";
 
 const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
+export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
   //Register user
-  const register = async ({username, email, password}) => {
+  const register = async ({ username, email, password }) => {
     console.log(username, email, password);
   };
 
   // Login user
-  const login = async ({email, password}) => {
-    console.log(email, password);
+  const login = async ({ email: identifier, password }) => {
+    const res = await fetch(`${NEXT_URL}/api/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        identifier,
+        password,
+      }),
+    });
+
+    const data = await res.json();
+    console.log(data);
+
+    if (res.ok) {
+    } else {
+      setError(data.message);
+    }
   };
 
   //logout user
@@ -29,9 +46,9 @@ export const AuthProvider = ({children}) => {
   };
 
   return (
-      <AuthContext.Provider value={{user, error, register, login, logout}}>
-        {children}
-      </AuthContext.Provider>
+    <AuthContext.Provider value={{ user, error, register, login, logout }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
